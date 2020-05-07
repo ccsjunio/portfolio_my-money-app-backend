5c858e66ba9f71d919cb2e233b53956cdd70982c
Carlos Cesar da Silva Ferraz Junior <carlos.cesar.ferraz@gmail.com>
2020-05-07 15:43:49 -0400

    added route and functionality to count the number of registers

--- src/config/database.js
+++ src/config/database.js
@@ -2,3 +2,12 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
 
+mongoose.Error.messages.general.required = "Attribute '{PATH}' is required"
+mongoose.Error.messages.Number.min = 
+  "The {PATH} '{VALUE}' informed is lower than the minimum of '{MIN}'."
+mongoose.Error.messages.Number.max = 
+  "The {PATH} '{VALUE}' informed is higher than the maximum of '{MAX}'."
+mongoose.Error.messages.String.enum = 
+  "'{VALUE}' is not valid for the attribute '{PATH}'."
+
+
